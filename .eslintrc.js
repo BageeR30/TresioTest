ed65cf193c2f400152421224c333927b1a075389
Pavel Permyakov <bageeer@gmail.com>
2025-01-09 12:21:26 +0500

    dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        'parser': '@babel/eslint-parser',
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+    },
+    plugins: [
+        'vue',
+    ],
+};
